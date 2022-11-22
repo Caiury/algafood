43c0715fb9388859d802e8157e34cf91c305b678
Caiury Henrique Da Silva <caiuryf@gmail.com>
2022-11-22 07:23:32 -0300

    add repository ao projeto

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -10,13 +10,13 @@ import javax.persistence.Id;
 
 @Entity
 public class Restaurante {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nome;
-	
+
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 
@@ -43,7 +43,30 @@ public class Restaurante {
 	public void setTaxaFrete(BigDecimal taxaFrete) {
 		this.taxaFrete = taxaFrete;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurante other = (Restaurante) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
---
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.domain.repository;
+
+import java.util.List;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+public interface CozinhaRepository {
+	
+	public List<Cozinha>listarCozinhas ();
+	public Cozinha buscarCozinha(Long id);
+	public Cozinha salvar(Cozinha cozinha);
+	public void remover (Cozinha cozinha);
+
+}
