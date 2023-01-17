72cff01cbafac227a33cf119a4bdbfde59afa0ca
Caiury Henrique Da Silva <caiuryf@gmail.com>
2023-01-16 23:31:06 -0300

    Usando as keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,7 +1,10 @@
 package com.algaworks.algafood.api.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,9 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
 //	@GetMapping("/cozinhas/por-nome")
 //	Cozinha buscarPorNome(@RequestParam String nome){
 //		return cozinhaRepository.findByNome(nome);
@@ -26,6 +32,17 @@ public class TesteController {
     List<Cozinha> buscarNome(@RequestParam String nome) {
         return cozinhaRepository.findByNomeContaining(nome);
     }
+
+    @GetMapping("/restaurante/por-taxa")
+    List<Restaurante> buscarTaxa(@RequestParam BigDecimal taxaInicial , BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial,taxaFinal);
+    }
+
+    @GetMapping("/restaurante/por-nome")
+    List<Restaurante> buscarNome(@RequestParam String nome, Long id) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome,id);
+    }
+
 
 
 }
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante,Long> {
+
+    public List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    public List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long id);
 
 }
